fcb3eee5d0babc694989f984af5bdf1e020be007
sshhsh <sshhsh@foxmail.com>
2021-12-15 00:36:53 +0800

    migrate to compose

--- app/src/main/java/science/kiddd/motionwallpaper/MainActivity.kt
+++ app/src/main/java/science/kiddd/motionwallpaper/MainActivity.kt
@@ -4,69 +4,77 @@ import android.app.WallpaperManager
 import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import com.arthenica.ffmpegkit.FFmpegKit
 import com.arthenica.ffmpegkit.FFmpegKitConfig
-import java.io.File
-import java.io.FileOutputStream
-import java.io.InputStream
+import science.kiddd.motionwallpaper.ui.theme.MotionWallpaperTheme
 
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            selectVideo()
-        }
-    }
-
-    private fun selectVideo() {
-        val intent = Intent().also {
-            it.type = "video/*"
-            it.action = Intent.ACTION_GET_CONTENT
-        }
-        videoResult.launch(intent)
-    }
-
-    var videoResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        if (result.resultCode == RESULT_OK) {
-            result.data?.data?.let {
-                val dir = getDir(dirName, MODE_PRIVATE)
-                for (f in dir.listFiles() ?: arrayOf()) {
-                    f.delete()
-                }
-                val path = FFmpegKitConfig.getSafParameterForRead(this, it)
-                FFmpegKit.executeAsync("-i $path -vf scale=w=540:h=1080:force_original_aspect_ratio=increase,crop=540:1080 -qscale:v 2 ${dir.absolutePath}/out-%04d.jpg") {
-                    val intent = Intent(
-                        WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER
-                    )
-                    intent.putExtra(
-                        WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
-                        ComponentName(this, WallPaper::class.java)
-                    )
-                    startActivity(intent)
+        setContent {
+            MotionWallpaperTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    ChooseVideoButton()
                 }
             }
         }
     }
 }
 
-fun copyStreamToFile(inputStream: InputStream, outputFile: File) {
-    inputStream.use { input ->
-        val outputStream = FileOutputStream(outputFile)
-        outputStream.use { output ->
-            val buffer = ByteArray(4 * 1024) // buffer size
-            while (true) {
-                val byteCount = input.read(buffer)
-                if (byteCount < 0) break
-                output.write(buffer, 0, byteCount)
+@Composable
+fun ChooseVideoButton() {
+    val context = LocalContext.current
+    val videoResult =
+        rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == AppCompatActivity.RESULT_OK) {
+                result.data?.data?.let {
+                    val dir = context.getDir(dirName, AppCompatActivity.MODE_PRIVATE)
+                    for (f in dir.listFiles() ?: arrayOf()) {
+                        f.delete()
+                    }
+                    val path = FFmpegKitConfig.getSafParameterForRead(context, it)
+                    FFmpegKit.executeAsync("-i $path -vf scale=w=540:h=1080:force_original_aspect_ratio=increase,crop=540:1080 -qscale:v 2 ${dir.absolutePath}/out-%04d.jpg") {
+                        val intent = Intent(
+                            WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER
+                        )
+                        intent.putExtra(
+                            WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+                            ComponentName(context, WallPaper::class.java)
+                        )
+                        context.startActivity(intent)
+                    }
+                }
             }
-            output.flush()
         }
+    Button(content = {
+        Text("Button")
+    },
+        onClick = {
+            val intent = Intent().also {
+                it.type = "video/*"
+                it.action = Intent.ACTION_GET_CONTENT
+            }
+            videoResult.launch(intent)
+        })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MotionWallpaperTheme {
+        ChooseVideoButton()
     }
 }
